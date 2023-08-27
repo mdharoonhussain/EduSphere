85ac08f0c5b234396bc00199c271c322fe5e4c8d
mdharoonhussain <haroonhussain97@gmail.com>
2023-08-27 23:19:29 +0530

    initial commit

---
+++ EduSphere/src/app/masai-edu/masai-edu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-masai-edu',
+  templateUrl: './masai-edu.component.html',
+  styleUrls: ['./masai-edu.component.css']
+})
+export class MasaiEduComponent {
+
+}
